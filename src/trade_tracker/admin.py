3f4e1c2a2f909b2b3a9d9da356d9f77b9da83105
Matt Larsen <utegrad@gmail.com>
2026-04-07 21:37:03 -0600

    Extend trade tracker with admin configuration and async task tracking
    - Registered `Listing` and `ListingUpload` models in Django Admin with custom configurations.
    - Added support for async task tracking in `ListingUpload` model.
    - Included `django_celery_results` for task result storage and configured in settings.
    - Added migrations for `accounts` and `trade_tracker` apps.
    - Updated example environment file and base requirements.

--- src/trade_tracker/admin.py
+++ src/trade_tracker/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.listing_upload import ListingUpload
+from .models.listing import Listing
+
+
+@admin.register(ListingUpload)
+class ListingsUploadAdmin(admin.ModelAdmin):
+    list_display = (
+        "trimmed_friendly_file_name",
+        "trimmed_description",
+        "imported",
+    )
+    search_fields = (
+        "listings_file",
+        "description",
+    )
+    readonly_fields = (
+        "latest_task_status",
+        "imported",
+        "latest_task_id",
+    )
+    fields = (
+        "listings_file",
+        "description",
+        "imported",
+        "latest_task_id",
+        "latest_task_status",
+    )
+
+
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "active", "country", "sector", "industry")
+    list_filter = (
+        "country",
+        "sector",
+        "industry",
+        "active",
+    )
+    readonly_fields = ("original_ticker",)
+    ordering = ("ticker",)
+    search_fields = (
+        "ticker",
+        "name",
+    )
+    fieldsets = (
+        (None, {"fields": ("name", "ticker", "active")}),
+        (
+            "About",
+            {
+                "classes": ("collapse",),
+                "fields": (
+                    "country",
+                    "sector",
+                    "industry",
+                    "original_ticker",
+                ),
+            },
+        ),
+    )
